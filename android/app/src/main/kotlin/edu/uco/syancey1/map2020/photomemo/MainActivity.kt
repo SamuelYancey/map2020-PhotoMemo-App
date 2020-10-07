7afef97442cadfd574956aabf12511184edf70aa
SamuelYancey <syancey1@uco.edu>
2020-10-07 12:58:19 -0500

    Initial Commit

---
+++ android/app/src/main/kotlin/edu/uco/syancey1/map2020/photomemo/MainActivity.kt
@@ -0,0 +1,6 @@
+package edu.uco.syancey1.map2020.photomemo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(PhotoMemoApp());
+}
+
+class PhotoMemoApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+
+}
