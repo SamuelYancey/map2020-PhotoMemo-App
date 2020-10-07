a0d35f8c076ff557b16e76f0bfd876843256c135
SamuelYancey <syancey1@uco.edu>
2020-10-07 13:17:22 -0500

    signIn visual fields implemented

--- lib/screens/signin_screen.dart
+++ lib/screens/signin_screen.dart
@@ -11,6 +11,7 @@ class SignInScreen extends StatefulWidget{
 class _SignInState extends State<SignInScreen>{
 
   _Controller con;
+  var formKey = GlobalKey<FormState>();
 
   @override
   void initState() {
@@ -24,7 +25,38 @@ class _SignInState extends State<SignInScreen>{
       appBar: AppBar(
         title: Text('Sign in'),
       ),
-      body: Text('Sign in'),
+      body: SingleChildScrollView(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+                autocorrect: false,
+                validator: con.validatorEmail,
+                onSaved: con.onSavedEmail,
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+                autocorrect: false,
+                validator: con.validatorPassword,
+                onSaved: con.onSavedPassword,
+              ),
+              RaisedButton(
+                child: Text('Sign In', style: TextStyle(fontSize: 25, color: Colors.white),),
+                color: Colors.teal,
+                onPressed: con.signIn,
+              ),
+            ]
+          ),
+        ),
+      ),
     );
   }
 
@@ -34,4 +66,24 @@ class _SignInState extends State<SignInScreen>{
 class _Controller{
   _SignInState _state;
   _Controller(this._state);
+
+  void signIn(){
+
+  }
+
+  String validatorEmail(String value){
+
+  }
+
+  void onSavedEmail(String value){
+
+  }
+
+  String validatorPassword(String value){
+
+  }
+
+  void onSavedPassword(String value){
+
+  }
 }
